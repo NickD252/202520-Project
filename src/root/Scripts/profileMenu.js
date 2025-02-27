db7d433459eed496ddfbb02e761289034b16855c
AHJS93 <123779946+AHJS93@users.noreply.github.com>
2025-02-27 03:27:39 -0600

    Changed design of profile menu -Created profileMenuStyles.css -Created profileMenu.js -Referenced style and script in Site.Master -Added style and functionality to menu

---
+++ src/root/Scripts/profileMenu.js
@@ -0,0 +1,35 @@
+﻿function openNav() {
+    if (document.getElementById("mySidenav").style.width == "20rem") {
+        document.getElementById("mySidenav").style.width = "0";
+        document.body.style.marginRight = "0";
+        document.body.style.backgroundColor = "white";
+        document.getElementById("subMenu").style.width = "0";
+    } else {
+        document.getElementById("mySidenav").style.width = "20rem";
+        document.body.style.marginRight = "20rem";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.marginRight = "0";
+    document.body.style.backgroundColor = "white";
+    document.getElementById("subMenu").style.width = "0";
+}
+
+function displayTerms() {
+    if (document.getElementById("subMenu").style.width == "15rem") {
+        document.getElementById("subMenu").style.width = "0";
+    } else {
+        document.getElementById("subMenu").style.width = "15rem";
+        document.getElementById("subMenu").style.marginRight = "20rem";
+    }
+}
+
+function closeAllMenus() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("subMenu").style.width = "0";
+    document.body.style.marginRight = "0";
+    document.body.style.backgroundColor = "white";
+}
