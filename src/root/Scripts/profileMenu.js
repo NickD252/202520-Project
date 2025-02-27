dc1ce2f50b2c9c514ac611e4c2e30484d312abe7
AHJS93 <123779946+AHJS93@users.noreply.github.com>
2025-02-27 04:04:38 -0600

    -Made profile menu links more readable

--- src/root/Scripts/profileMenu.js
+++ src/root/Scripts/profileMenu.js
@@ -6,11 +6,12 @@ function openNav() {
     if (sidebar.style.width == "20rem") {
         sidebar.style.width = "0";
         bdstyle.marginRight = "0";
+        bdstyle.transition = "0.3s";
         bdstyle.backgroundColor = "white";
         submenu.style.width = "0";
     } else {
         sidebar.style.width = "20rem";
-        bdstyle.transition = "0.5s";
+        bdstyle.transition = "0.3s";
         bdstyle.marginRight = "20rem";
         bdstyle.backgroundColor = "rgba(0,0,0,0.4)";
     }
@@ -20,14 +21,15 @@ function closeNav() {
     sidebar.style.width = "0";
     bdstyle.marginRight = "0";
     bdstyle.backgroundColor = "white";
+    bdstyle.transition = "0.3s";
     submenu.style.width = "0";
 }
 
 function displayTerms() {
-    if (submenu.style.width == "15rem") {
+    if (submenu.style.width == "20rem") {
         submenu.style.width = "0";
     } else {
-        submenu.style.width = "15rem";
+        submenu.style.width = "20rem";
         submenu.style.marginRight = "20rem";
     }
 }
@@ -36,5 +38,6 @@ function closeAllMenus() {
     sidebar.style.width = "0";
     submenu.style.width = "0";
     bdstyle.marginRight = "0";
+    bdstyle.transition = "0.3s";
     bdstyle.backgroundColor = "white";
 }
