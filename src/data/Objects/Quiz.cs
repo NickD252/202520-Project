dda6a19d7de6b6cbc57d36f6df6a25f3b759dc44
jwood36 <53241126+jwood36@users.noreply.github.com>
2025-03-17 09:03:57 -0500

    Refactored Quiz to Inherit from QuizView

--- src/data/Objects/Quiz.cs
+++ src/data/Objects/Quiz.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 
-namespace coding_lms.data {
-	public class Quiz {
-
+﻿namespace coding_lms.data {
+	public class Quiz : QuizView {
 		public Quiz(int term, string crn, string name) : this() {
 			this._term = term;
 			this._crn = crn;
-			this._name = name;
+			this.Name = name;
 		}
 		private Quiz() {
 			this.Init();
@@ -22,24 +21,6 @@ namespace coding_lms.data {
 			set { _id = value; }
 		}
 
-		private Guid _uuid;
-		public Guid UUID {
-			get {
-				if ( this._uuid == Guid.Empty ) {
-					this._uuid = Guid.NewGuid();
-				}
-
-				return _uuid;
-			}
-			set { _uuid = value; }
-		}
-
-		private bool _random;
-		public bool IsRandom {
-			get { return _random; }
-			set { _random = value; }
-		}
-
 		private int _term;
 		public int Term {
 			get { return _term; }
@@ -52,23 +33,10 @@ namespace coding_lms.data {
 			set { this._crn = value; }
 		}
 
-		private string _name;
-		public string Name {
-			get { return _name; }
-			set { this._name = value; }
-		}
-
 		private int _time;
 		public int Time {
 			get { return this._time; }
 			set { this._time = value; }
-		}
-
-		private int _ppq;
-
-		public int PPQ {
-			get { return _ppq; }
-			set { _ppq = value; }
 		}
 
 	}
