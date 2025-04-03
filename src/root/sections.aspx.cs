c48aee98456d3de1802eabd1fa24abafc23d5920
Gamer-Named-Ian <ianjohnson005@gmail.com>
2025-04-03 00:28:53 -0500

    begining work

--- src/root/sections.aspx.cs
+++ src/root/sections.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using coding_lms.data;
 
 namespace coding_lms
 {
@@ -11,7 +12,10 @@ namespace coding_lms
 	{
 		protected void Page_Load(object sender, EventArgs e)
 		{
+			if (!IsPostBack)
+			{
 
+			}
 		}
 	}
 }
