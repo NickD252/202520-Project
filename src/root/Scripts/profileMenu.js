0ccaaaf5fc321870cb31a5d83601dd77295f995a
AHJS93 <123779946+AHJS93@users.noreply.github.com>
2025-04-03 16:19:22 -0500

    -Consolidated profileMenu.js into JQuery syntax and into a single function Fixes #30

--- src/root/Scripts/profileMenu.js
+++ src/root/Scripts/profileMenu.js
@@ -3,54 +3,33 @@ const sidebar = document.getElementById("profileMenuNav");
 const submenu = document.getElementById("profileSubMenu");
 const bdstyle = document.body.style;
 
-document.getElementById("menuBtn").addEventListener("click", function ()
-{
-    if (sidebar.style.width == "13rem")
-    {
-        sidebar.style.width = "0";
-        bdstyle.marginRight = "0";
-        bdstyle.transition = "0.3s";
-        bdstyle.backgroundColor = "white";
-        submenu.style.width = "0";
-    } else
-    {
-        sidebar.style.width = "10rem";
-        bdstyle.transition = "0.3s";
-        bdstyle.marginRight = "0";
-    }
-})
-
-
-function closeNav() {
-    sidebar.style.width = "0";
-    bdstyle.marginRight = "0";
-    bdstyle.backgroundColor = "white";
-    bdstyle.transition = "0.3s";
-    submenu.style.width = "0";
-    submenu.style.transitionDuration = "0ms";
-}
-
-function displayTerms() {
-    if (submenu.style.width == "10rem") {
-        submenu.style.width = "0";
-        submenu.style.transitionDuration = "0ms";
-    } else {
-        submenu.style.width = "10rem";
-        submenu.style.marginLeft = "10rem";
-        submenu.style.transitionDuration = "0ms";
-    }
-}
-
-function closeAllMenus() {
-    sidebar.style.width = "0";
-    submenu.style.width = "0";
-    bdstyle.marginRight = "0";
-    bdstyle.transition = "0.3s";
-    bdstyle.backgroundColor = "white";
-}
-
 $(document).ready(function ()
 {
+    $("#menuBtn").on("click", function ()
+    {
+        if (sidebar.style.width == "13rem")
+        {
+            sidebar.style.width = "0";
+            bdstyle.marginRight = "0";
+            bdstyle.transition = "0.3s";
+            bdstyle.backgroundColor = "white";
+            submenu.style.width = "0";
+        } else
+        {
+            sidebar.style.width = "10rem";
+            bdstyle.transition = "0.3s";
+            bdstyle.marginRight = "0";
+        }
+    })
+
+    $(".body-content").on("click", function ()
+    {
+        sidebar.style.width = "0";
+        submenu.style.width = "0";
+        bdstyle.marginRight = "0";
+        bdstyle.transition = "0.3s";
+    })
+
     $("#menuTerms").hover(
         function ()
         {
@@ -86,25 +65,20 @@ $(document).ready(function ()
             $("#profileSubMenu").css("margin-left", "0rem");
         }
     );
-})
 
-//Redirection from menu links
-$(document).ready(function () {
-    $("#menuQuizzes").on("click", function () {
+    $("#menuQuizzes").on("click", function ()
+    {
         window.location.href = "/quizzes";
     });
-});
 
-$(document).ready(function () {
-    $("#menuCalendar").on("click", function () {
+    $("#menuCalendar").on("click", function ()
+    {
         window.location.href = "/calendar";
     });
-});
 
-//Storage of selected Term from link
-$(document).ready(function () {
-    $("#profileSubMenu > a").on("click", function () {
+    $("#profileSubMenu > a").on("click", function ()
+    {
         var sessionMenuTerm = $(this).text();
         sessionStorage.setItem("selectedTerm", sessionMenuTerm);
     });
-});
+})
