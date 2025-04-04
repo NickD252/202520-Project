01ed509833482f8d209cfd575e4584a43e1ead06
AHJS93 <123779946+AHJS93@users.noreply.github.com>
2025-04-04 14:57:05 -0500

    -Consolidated some colors on profileMenuStyles -Edited sections code-behind page_load -Minor changes to profile menu working on paths

--- src/root/Scripts/sectionsview.js
+++ src/root/Scripts/sectionsview.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
     $("#sectionViewTable tr:gt(0)").on('dblclick', function () {
         var sectionId = $(this).find("td:nth-child(3)").text();
-        window.location.href = "enrollment/sct_" + sectionId;
-       
+        window.location.href = "sections/enrollment/sct_" + sectionId;
     });
 });
-
