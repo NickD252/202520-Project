efcf8eeec0cb180b858a46a9fa65755295ecc785
ttidwell19 <timtidwell19@gmail.com>
2025-04-03 22:47:40 -0500

    Created method to check for enrollment on page load created data bind for enrollment in repeater.

--- src/root/Enrollment.aspx.cs
+++ src/root/Enrollment.aspx.cs
@@ -1,9 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// Added reference to data and quiz in the root, Adding coding_lms.data.
+using coding_lms.data;
 
 namespace coding_lms {
-	public partial class _Default : Page {
-		protected void Page_Load ( object sender , EventArgs e ) {
+
+	public partial class Enrollment : System.Web.UI.Page {
+
+		protected void Page_Load(object sender, EventArgs e) {
+
+			// Add condition to check for enrollment on page load.
+			if (!IsPostBack)
+			{
+				var InstructorDB instructorDb = new InstructorDB();
+				var IEnumurable<EnrollmentView> Enrollment = instructorDb.GetEnrollmentViews();
+
+				// Data bind enrollment in a repeater.
+				enrollmentRepeater.DataSource = Enrollment;
+				enrollmentRepeater.DataBind();
+			}
 
 		}
 	}
