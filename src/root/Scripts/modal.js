3c0ae8699b6011e8abd58efefd8c005d006f8654
Claiborne Spencer <cspence4@jeffersonstate.edu>
2025-04-18 20:07:35 -0500

    Issue #28 closing
    Refactored for jQuery, issue 28 should be resolved

--- src/root/Scripts/modal.js
+++ src/root/Scripts/modal.js
@@ -10,12 +10,11 @@ function closeModal() {
 
 // Function to save question when the "Save Question" button is clicked
 function saveQuestion() {
+    // Use jQuery to get values from the form
     var title = $("#title").val();
     var body = $("#body").val();
     var type = $("#type").val();
     var options = $("#options").val();
-
-    // TODO: You can handle form data submission here via AJAX if needed
 
     // Close the modal after saving
     closeModal();
